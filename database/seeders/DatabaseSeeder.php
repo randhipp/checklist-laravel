0cab8fc1a08bb2e315ba1a489089601d45ef5291
Randhi Pratama Putra <randhi.pp@gmail.com>
2020-11-18 22:48:00 +0700

    Sanctum initial setup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Log;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Log::channel('stderr')->info("Creating 10 dummy user...");
+        User::factory(10)->create();
+        foreach(User::all() as $user){
+            $token = $user->createToken('default-token');
+            Log::channel('stderr')->info("Token created for $user->email -> $token->plainTextToken");
+        }
+        Log::channel('stderr')->info("User created!");
     }
 }
