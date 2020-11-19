b2a72d7c26dfcb7cefb6963a706a5208d8e4b245
Randhi Pratama Putra <randhi.pp@gmail.com>
2020-11-19 14:55:18 +0700

    History API - Add Logable for items and checklist Fixes #3

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -5,10 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use RichanFongdasen\EloquentBlameable\BlameableTrait;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Item extends Model
 {
-    use HasFactory, BlameableTrait;
+    use HasFactory, BlameableTrait, LogsActivity;
+
+    protected static $logAttributes = ['*'];
 
     protected $hidden = [
         'id','created_at','updated_at'
